b17ad811a3bd0e838538090596697ecc03d89c7c
kiven <984768569@qq.com>
2022-07-19 10:26:27 +0800

    *

--- UtilsLibrary/src/main/kotlin/com/kiven/jutil/KUtil.kt
+++ UtilsLibrary/src/main/kotlin/com/kiven/jutil/KUtil.kt
@@ -4,5 +4,8 @@ fun String.isPhone() = matches(Regex("1\\d{10}"))
 fun String.isIP() = matches(Regex("((2(5[0-5]|[0-4]\\d))|[0-1]?\\d{1,2})(\\.((2(5[0-5]|[0-4]\\d))|[0-1]?\\d{1,2})){3}"))
 
 object KUtil {
+    fun computeByteLong(length: Long): String {
+        return length.toString()
+    }
 
 }
